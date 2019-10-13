77dcf5417bc94e7a2cf85e18f06894ebe592b862
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-10-13 09:01:43 +0800

    增加表单验证

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 加载验证插件的语言包
+import * as rules from 'vee-validate/dist/rules'
+
+import './vant'
+
+// 配置使用中文语言
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+
+// 注册为全局组件
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 Vue.config.productionTip = false
 
