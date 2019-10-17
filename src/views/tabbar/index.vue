ee7ea5e8680a454102da1ac5daa6668860fb96db
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-10-17 19:20:42 +0800

    修改tabbar 增加登录与未登录的校验

--- src/views/tabbar/index.vue
+++ src/views/tabbar/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <router-view></router-view>
-
-    <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="search">问答</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+    <!-- 增加tabbar跳转 -->
+    <van-tabbar route>
+      <van-tabbar-item replace to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item replace icon="search">问答</van-tabbar-item>
+      <van-tabbar-item replace icon="friends-o">视频</van-tabbar-item>
+      <!-- <van-tabbar-item replace to="/mine" v-if="$store.state.user" icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item replace to="/mine" v-else icon="setting-o">未登录</van-tabbar-item> -->
+      <van-tabbar-item icon="setting-o" to="/mine">{{ $store.state.user ? '我的' : '未登录'}}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
